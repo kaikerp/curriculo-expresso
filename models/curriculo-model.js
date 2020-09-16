238bc4d9605641c639ad3461b830f22b7f96500c
Kaike Ribeiro Pereira <kaikerp@gmail.com>
2020-09-16 17:41:09 -0300

    projeto update 2.0

---
+++ models/curriculo-model.js
@@ -0,0 +1,27 @@
+const data = {
+    title: 'Meu currículo',
+        name: 'Kaike Ribeiro Pereira',
+        profession: 'Designer Software Engineer',
+        description: 'Possuo sólidos conhecimentos em criação de mídia offline e online, social media, motion graphics, edição de video e fotografia, UX e UI Design. Já trabalhei como assistente de gestão de projeto e de marketing, e também como Designer, onde eu era responsável por toda a criação! Atualmente estou realizando um curso de desenvolvimento Full Stack para aprender a programar!',
+        experience: [{
+            company: 'Grupo Brugnara',
+            office: 'Designer Pleno',
+            description: 'Responsável por toda a criação de mídia online e offline demais de 5 empresas do Grupo Brugnara Entre os materiais que eu produzia estão: artes para social media, vídeos, propagandas,apresentações, material de prospecção das empresas, papelaria em geral e comunicação visual para diversas feiras ao redor do Brasil.'
+        },
+        {
+            company: 'Giungla',
+            office: 'Assistente de Gestão de projetos',
+            description: 'Fui Assistente de Gestão de projetos criativos, onde eu realiza atendimento ao cliente, debriefing dos projetos, além da programação de email marketing, artes para newsletters, banners, gerenciamento dos sites via CMS. Nós cuidávamos das contas da Clinique, Jo Malone, La Mer e MAC .'
+        }],
+        education: [{
+            institution: 'Digital House',
+            description: 'Desenvolvimento Full Stack'
+        }],
+        education2: [{
+            institution: 'Anhembi Morumbi',
+            description: 'Design Gráfico'
+        }],
+        skills: ['Backend', 'Frontend', 'Infra', 'Mobile','UX', 'UI', 'Design']
+}
+
+module.exports = data;
